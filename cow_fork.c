731dfbfb2d3b563c9af92b9f78973da56bb0af6c
Wu Xingbo <wuxb45@gmail.com>
2019-10-16 16:04:37 -0500

    hw5: bonus task cow_fork

---
+++ cow_fork.c
@@ -0,0 +1,25 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char * argv[])
+{
+  printf(1, "Before malloc: %d\n", freepages());
+  char * x = malloc(500000);
+  memset(x, '?', 500000);
+  printf(1, "After malloc: %d %c\n", freepages(), x[100000]);
+
+  if (fork() == 0) {
+    printf(1, "Child: Before memset: %d\n", freepages());
+    memset(x, 'C', 500000);
+    printf(1, "Child: After  memset: %d %c\n", freepages(), x[100000]);
+  } else {
+    sleep(3);
+    printf(1, "Parent: Before memset: %d\n", freepages());
+    memset(x, 'P', 500000);
+    printf(1, "Parent: After  memset: %d %c\n", freepages(), x[100000]);
+    wait();
+  }
+  exit();
+}
