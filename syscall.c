950bc3a077a52d9eab99fdc63683117a274f24a1
Jakob Eriksson <jakob@uic.edu>
2024-11-18 11:53:48 -0600

    dropped extra killed check at top of syscall

--- syscall.c
+++ syscall.c
@@ -151,8 +151,6 @@ static addr_t (*syscalls[])(void) = {
 void
 syscall(struct trapframe *tf)
 {
-  if (proc->killed)
-    exit();
   proc->tf = tf;
   uint64 num = proc->tf->rax;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
