b6a80116807b18c13a50f7961bf5a9414993b53a
xjtuwxg <xjtuwxg@gmail.com>
2026-03-10 15:43:52 -0500

    Changed to a simplified version of alarmtest3.c

--- alarmtest3.c
+++ alarmtest3.c
@@ -3,34 +3,25 @@
 
 int snoozetime = 5;
 void dummy() {
-  printf(1,"This function is only here so snooze doesn't end up at address 0!\n");
+  printf(1, "This function is only here so snooze doesn't end up at address 0!\n");
 }
 
 void snooze(int signum) {
-  // this function deliberately overwrites a register
-  // value. The main function keeps important data in
-  // %rbx, and in correctly implemented signal handling,
-  // that data should not be overwritten by this bit of asm
-  asm volatile("xor %%ebx,%%ebx":::);
-
-  printf(1,"Yawn... another %d seconds?\n",snoozetime--);
-  if(snoozetime==0)
-    signal(14,0);
+  printf(1, "Yawn... another %d seconds?\n", snoozetime--);
+  if(snoozetime == 0)
+    signal(14, 0);
   alarm(1);
 }
 
 int main(int argc, char** argv) {
-  signal(14,snooze);
+  signal(14, snooze);
   alarm(3);
 
-  // "register" forces i and j to reside in registers
-  // rather than on the stack. See snooze().
-  register int i=0,j=0;
-
-  while(1) {
-    printf(1,"Still looping %d, %d...\n",
-      i++,j++);
+  int i = 0;
+  while(i++ < 100) {
+    printf(1, "Looping... %d\n", i);
     sleep(100);
   }
+  exit();
 }
 
